903fa03c9a001ad72536f3f97cbae15088758388
bhawesh gupta <bhawesh1996@outlook.com>
2020-07-05 06:17:00 +0530

    intial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstDotNetCoreApplication.Models;
+using MyFirstDotNetCoreApplication.Services;
+
+namespace MyFirstDotNetCoreApplication.Controllers
+{
+    [Route("con")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+
+        }
+      
+
+        public class RatingRequest
+        {
+            public string ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Pages/Products.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MyFirstDotNetCoreApplication.Models;
+using MyFirstDotNetCoreApplication.Services;
+
+namespace MyFirstDotNetCoreApplication.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly ILogger<ProductsModel> _logger;
+        public JsonFileProductService jsonFileProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public ProductsModel(ILogger<ProductsModel> logger,JsonFileProductService productService)
+        {
+            _logger = logger;
+            jsonFileProductService = productService;
+
+        }
+
+
+        public void OnGet()
+        {
+            Products = jsonFileProductService.GetProducts();
+        }
+    }
+}
